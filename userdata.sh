935cb7f37821bf6af44d60c78599241a014a044d
Pavel Yarema <pavel@1strategy.com>
2017-09-11 13:46:16 -0700

    Fix rabbitmq lvm volume.
    Add apt-get upgrade.

--- userdata.sh
+++ userdata.sh
@@ -2,12 +2,14 @@
 set -o errexit
 
 apt-get -y update
+apt-get -y upgrade 
+
 
 # Create volume for Rabbit messages and queues
 pvcreate /dev/xvdf
 vgcreate vg0 /dev/xvdf
-lvcreate -l 100%FREE -n myapp vg0
-mkfs.ext4 /dev/vg0/myapp
+lvcreate -l 100%FREE -n rabbitmq vg0
+mkfs.ext4 /dev/vg0/rabbitmq
 mkdir /var/lib/rabbitmq/
-echo "/dev/mapper/vg0-myapp /var/lib/rabbitmq ext4 defaults 0 2" >> /etc/fstab
+echo "/dev/mapper/vg0-rabbitmq /var/lib/rabbitmq ext4 defaults 0 2" >> /etc/fstab
 mount -a
