5c37ef4c6cabaf0e60a0506b63ca2a5e73715a14
Pavel Yarema <pavel@1strategy.com>
2017-09-11 14:00:23 -0700

    Install RabbitMQ via userdata

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-apt-get -y update
-apt-get -y upgrade 
-
 
 # Create volume for Rabbit messages and queues
 pvcreate /dev/xvdf
@@ -13,3 +10,16 @@ mkfs.ext4 /dev/vg0/rabbitmq
 mkdir /var/lib/rabbitmq/
 echo "/dev/mapper/vg0-rabbitmq /var/lib/rabbitmq ext4 defaults 0 2" >> /etc/fstab
 mount -a
+
+
+cat <<EOF > /etc/apt/sources.list.d/rabbitmq.list
+deb http://www.rabbitmq.com/debian/ testing main
+EOF
+
+curl https://www.rabbitmq.com/rabbitmq-release-signing-key.asc -o /tmp/rabbitmq-release-signing-key.asc
+apt-key add /tmp/rabbitmq-release-signing-key.asc
+rm /tmp/rabbitmq-release-signing-key.asc
+
+apt-get -qy update
+apt-get -y dist-upgrade 
+apt-get -qy install rabbitmq-server
