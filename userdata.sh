1082bd07f44038f848e376404c817a6cde480131
Doug Ireton <doug@1strategy.com>
2017-09-12 11:42:39 -0700

    Set Erlang cookie and USE_LONGNAME in userdata

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-
+set -o nounset
 
 # Create volume for Rabbit messages and queues
 pvcreate /dev/xvdf
@@ -11,7 +11,6 @@ mkdir /var/lib/rabbitmq/
 echo "/dev/mapper/vg0-rabbitmq /var/lib/rabbitmq ext4 defaults 0 2" >> /etc/fstab
 mount -a
 
-
 cat <<EOF > /etc/apt/sources.list.d/rabbitmq.list
 deb http://www.rabbitmq.com/debian/ testing main
 EOF
@@ -21,8 +20,21 @@ apt-key add /tmp/rabbitmq-release-signing-key.asc
 rm /tmp/rabbitmq-release-signing-key.asc
 
 apt-get -qy update
-apt-get -y dist-upgrade 
+apt-get -y dist-upgrade
+
 apt-get -qy install rabbitmq-server
-
-
 rabbitmq-plugins enable rabbitmq_management
+
+rabbitmqctl stop-app
+
+# Use FQDNs for RabbitMQ clustering
+echo "USE_LONGNAME=true" >> /etc/rabbitmq/rabbitmq-env.conf
+
+# Set Erlang cookie for RabbitMQ clustering
+echo "EHLKUCICVURANTZUDLJG" > /var/lib/rabbitmq/.erlang.cookie
+chmod 0400 /var/lib/rabbitmq/.erlang.cookie
+chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
+rabbitmqctl start_app
+
+# Reboot if kernel updates require it
+[[ -f "/var/run/reboot-required" ]] && shutdown -r now
