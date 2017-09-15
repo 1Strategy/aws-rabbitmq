c094da5f268d5a5f0782aaba7ead741cee3ed6d7
Pavel Yarema <pavel@1strategy.com>
2017-09-14 17:21:12 -0700

    Add LICENSE header to all files.
    Move declared variables in autoscaling.tf to variables.tf for consistency.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+# Copyright 2017 Zulily, LLC
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 variable "vpc" {
   type = "string"
   description = "Create RabbitMQ servers in this AWS VPC"
@@ -23,3 +28,15 @@ variable "team" {
     description = "Zulily team"
 }
 
+variable "ebs_disk_size" {
+    type = "string"
+    description = "EBS volume size (in GB) that is attached to the RabbitMQ node."
+}
+variable "asg_min" {
+    type = "string"
+    description = "Minimum number of nodes in the Auto-Scaling Group"
+}
+variable "asg_max" {
+    type = "string"
+    description = "Minimum number of nodes in the Auto-Scaling Group"
+}
